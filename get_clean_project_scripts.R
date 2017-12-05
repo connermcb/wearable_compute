f4386f82ea57d3878e97c06f1dc5e9d23c466d19
connermcb <connermcb@yahoo.com>
2017-12-05 12:43:54 -0600

    change activity code to descriptive name

--- get_clean_project_scripts.R
+++ get_clean_project_scripts.R
@@ -10,33 +10,41 @@ library(stringi)
 vars <- readLines("features.txt")
 vars <- unlist(lapply(vars, function(x){gsub("^[0-9]+ *", "", x)}))
 
+# Load activity labels
+actvs <- read.table("activity_labels.txt",
+                    header=FALSE, col.names = c("activity", "activity_name"))
+
 ## Get and join data sets
 # Load train datasets
 train_x <- read.table("train/X_train.txt", 
                       header=FALSE, col.names = vars)
 train_y <- read.table("train/y_train.txt", 
                       header=FALSE, col.names = "activity")
+activity_name <- merge(train_y, actvs, by="activity")[, 2] # describe activities
+
 train_subject <- read.table("train/subject_train.txt", 
                             header=FALSE, col.names = "subject")
 
 # bind x and y, train
-train <- cbind(train_subject, train_y, train_x)
+train <- cbind(train_subject, activity_name, train_x)
 
 # Load test datasets
 test_x <- read.table("test/X_test.txt", 
                      header=FALSE, col.names = vars)
 test_y <- read.table("test/y_test.txt", 
                      header=FALSE, col.names = "activity")
+activity_name <- merge(test_y, actvs, by="activity")[, 2] # describe activities
 test_subject <- read.table("test/subject_test.txt", 
                            header=FALSE, col.names = "subject")
 
 # bind x and y, test
-test <- cbind(test_subject, test_y, test_x)
+test <- cbind(test_subject, activity_name, test_x)
 
 # bind train and test sets
 full_data <- rbind(train, test)
 
 ## Subset data on mean and standard deviation
+full_data <- full_data[, -(516:561)]
 full_data <- full_data[, grepl("std|mean|subject|activity", names(full_data))]
 full_data <- full_data[, !grepl("Freq", names(full_data))]
 
@@ -60,7 +68,7 @@ names(full_data) <- vars_list
 ## summarize data
 # group and get mean by group
 wide_data <- full_data%>%
-  group_by(subject, activity)%>%
+  group_by(subject, activity_name)%>%
   summarize_all(funs(mean))
 
 # write wide summary table to file
